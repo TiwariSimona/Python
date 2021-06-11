79ee1710b0d525add4c2dc84291374a8bcfa32a8
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-11 10:49:43 +0530

    Create FuncLength.py

---
+++ FuncLength.py
@@ -0,0 +1,7 @@
+# Write a function which receive two strings as arguments and check whether they are of same length or not. (true/false)
+
+def func(S1,S2):
+  if (len(S1) == len(S2):
+      return True
+   else:
+      return False
