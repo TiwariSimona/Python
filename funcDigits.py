e9fac63170fe21b02aad553a76f03c0a2876eb4d
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-11 10:55:38 +0530

    Create funcDigits.py

---
+++ funcDigits.py
@@ -0,0 +1,12 @@
+# Write a function which two numbers and returns the number that has minimum one's digit.
+
+def func(n1,n2):
+  O1 = n1%10
+  O2 = n2%10
+  if (O1<O2):
+    print('First Numbers\'s one\'s digit is small.')
+  if (O2>O1):
+    print ('Second Numbers\'s one\'s digit is small.')
+  if ( O2 = O1):
+    print('Both are equal')
+func()
