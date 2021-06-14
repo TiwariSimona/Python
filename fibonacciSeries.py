4d709bdfd1e13d34d70dca9f8e52a9140e268655
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-14 12:16:45 +0530

    Create fibonacciSeries.py

---
+++ fibonacciSeries.py
@@ -0,0 +1,9 @@
+#Write  program to print fibonacci series.
+
+def fibo(n):
+  if (n<=i):
+    return 1
+  else:
+    return (fibo (n-1) + fibo (n-2))
+  for i in range(n):
+    print (fibo(i),end = ' ')
