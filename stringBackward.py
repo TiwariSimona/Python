1ddb8a23666cfb0c6bda25f694c97f7056296fef
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-14 12:21:12 +0530

    Create stringBackward.py

---
+++ stringBackward.py
@@ -0,0 +1,15 @@
+# write a program to print a string backwards
+
+#iteration
+S = 'simona'
+L = len(S)
+for i in range (L-1,-1, -1):
+  print (S[i], end = ' ')
+  
+#Recursion
+S = 'simona'
+def rev(S):
+  if (len(S)==0):
+    return S
+  else:
+    return ( S[1: ])+S[0]
