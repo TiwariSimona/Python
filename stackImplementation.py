d8e43dd761844569b96a654b9b96a2d9ab4e6ceb
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-17 07:55:31 +0530

    Update stackImplementation.py

--- stackImplementation.py
+++ stackImplementation.py
@@ -33,8 +33,48 @@ def peek(stk):
     print('String is empty!')
   else:
     top=len(stk)-1
-    print(stk[top],'<---top')
+     print(stk[top],'<---top')
     for i in range (top-1,-1,-1):
       print (stk[i])
+      
+print('Stack Implementation') 
+print('1.push')
+print('2.pop')
+print('3.peek')
+print('4.Display')
+print('5.Exit')
+ 
+ch=int(input('Enter the choice between 1 and 5')
+  if (ch==1):
+       item = (input('Enter the item you want to push'))
+       push(5,item)
+       print('% added successfully ' %item)
+       input('Press any key to continue')
+  elif(ch==2):
+       item=s_pop(s)
+       if (item=='underflow):
+           print('underflow!stack is empty')
+       else:
+           print('%d is popped' %item)
+       input('Press any key to continue')
+  elif(ch==3):
+           item==peek(s)
+           if (item=='underflow'):
+            print ('Underflow!Stack is empty')
+           else:
+            print('%d is at the top'%item)
+           input ('Press any key to continue')
+   elif(ch==4):
+           display(s)
+           input('Press any to continue')
+   elif(ch==5):
+           break
+   else:
+           print('Enter between 1 & 5')
+           input('Press any to continue')
+  print('/n/n/n')
+           
+           
+    
       
       
