13b62bbb3cdaa22919f2250eeec24cb36ad757fd
Hi ,I'm Suchismita !!! <92647515+suchismita-007@users.noreply.github.com>
2021-10-17 15:17:12 +0530

    Update stringBackward.py

--- stringBackward.py
+++ stringBackward.py
@@ -7,18 +7,8 @@ for i in range (L-1,-1, -1):
   print (S[i], end = ' ')
   
 #Recursion
-S = 'simona'
-def rev(S):
-  if (len(S)==0):
-    return S
-  else:
-    return ( S[1: ])+S[0]
- 
-
-
-#Recursion2
 def rev(s):'suchismita'
 	if(len(s)==0):
 		return 1
-		else:
-			return rev(s[1:])+s[0]
+	else:
+		return rev(s[1:])+s[0]
