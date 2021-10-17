b9c1fe75211b0a8f0a52fde5763cb6715be53f69
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-10-17 15:11:43 +0530

    Update stringBackward.py

--- stringBackward.py
+++ stringBackward.py
@@ -6,10 +6,4 @@ L = len(S)
 for i in range (L-1,-1, -1):
   print (S[i], end = ' ')
   
-#Recursion
-/*S = 'simona'
-def rev(S):
-  if (len(S)==0):
-    return S
-  else:
-    return ( S[1: ])+S[0]*/
+
